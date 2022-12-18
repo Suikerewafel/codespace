3cbce4a2afa0cdce6fe1ff90a3221f555dcc3522
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-18 14:07:26 +0000

    Sun, Dec 18, 2022, 2:07 PM +00:00

--- parity.py
+++ parity.py
@@ -7,7 +7,7 @@ except:
     print("Dit is geen geheel getal en kan dus niet even zijn")
     exit()
 
-if n % 2 = 0:
+if n % 2 == 0:
     print("Het getal is even")
 else:
     print("Het getal is oneven")
