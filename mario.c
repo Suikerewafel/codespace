f091f471891863ba19331cc33acbb5db1b82313c
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-18 10:30:44 +0000

    Sun, Dec 18, 2022, 10:30 AM +00:00

--- mario.c
+++ mario.c
@@ -12,7 +12,7 @@ int main(void)
 
     for( int i = 0; i < h; i++)
     {
-        for (int j= h - i; j = h; j++)
+        for (int j= h - i; j <= h; j++)
         {
             printf("#");
         }
