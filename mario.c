2f9fb14715133b6520a9ca0c39c80bd726ddb02e
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-18 07:35:38 +0000

    Sun, Dec 18, 2022, 7:35 AM +00:00

--- mario.c
+++ mario.c
@@ -3,5 +3,16 @@
 
 int main(void)
 {
-//
+    // ask user for height
+
+int n = get_int("breedte: ");
+
+
+
+    // print the asked #
+
+    for(i = 0; i < n; i++)
+    {
+        printf("#\n");
+    }
 }
