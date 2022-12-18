901ee84017d73aa4c412a9c1aaec28976c775de8
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-18 09:54:58 +0000

    Sun, Dec 18, 2022, 9:54 AM +00:00

--- mario.c
+++ mario.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void pyramide (int n);
+
 int main(void)
 {
     // ask user for size between 1 and 8
@@ -10,8 +12,11 @@ do
     n = get_int("geef een hoogte in tussen 1 en 8: ");
 }
 while (n < 1 || n > 8 );
+}
 
     // pyramide maken
+void pyramide (int n)
+{
 for (int i = 0; i < n; i++)
 {
     for(int j = 0; j < (i + 1); j++)
