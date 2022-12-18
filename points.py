90f0d8de6b2b76455449224f41be2e93f8466aa8
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-18 13:49:53 +0000

    Sun, Dec 18, 2022, 1:49 PM +00:00

--- points.py
+++ points.py
@@ -1,1 +1,9 @@
-points = int()
+points = int(("hoeveel punten heb je verloren?"))
+mypoints = int(5)
+
+if points > mypoints:
+    print("winner winner chicken dinner")
+elif points < mypoints:
+    print("loser!")
+else:
+    print("we are brothers")
