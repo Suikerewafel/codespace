d69b1cd8f9f1eb1e4682fa903522f8b80edcd2a3
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-18 10:15:14 +0000

    Sun, Dec 18, 2022, 10:15 AM +00:00

--- mario.c
+++ mario.c
@@ -1,32 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void pyramide (int n);
-
 int main(void)
 {
-    // ask user for size between 1 and 8
-int n;
-do
-{
-    n = get_int("geef een hoogte in tussen 1 en 8: ");
-}
-while (n < 1 || n > 8 );
-}
+    int h;
+    do
+    {
+        h = get_int("hoogte: ");
+    }
+    while( h < 1 || h > 8);
 
-    // pyramide maken
-void pyramide (int n)
-{
-    for (int i = 0; i < n; i++)
+    for( int i = 0; i < h; i++)
     {
-        for ( int k = n - i - 2; k <= 0; k--)
-    {
-        printf(" ");
-    }
-    for(int j = 0; j < (i + 1); j++)
-    {
-        printf("#");
-    }
-    printf("\n");
+        for (int j = h - i; j > 0; j--)
+        {
+            printf("#");
+        }
+        for (int k = 0; k > i; k++)
+        {
+            printf(" ");
+        }
+        printf("\n");
     }
 }
