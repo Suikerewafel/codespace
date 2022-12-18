3c9f34a9ea6c7ecb5b78cd33d943890b35bff44f
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-18 10:12:03 +0000

    Sun, Dec 18, 2022, 10:12 AM +00:00

--- mariomore.c
+++ mariomore.c
@@ -3,5 +3,29 @@
 
 int main(void)
 {
-    
+    int h;
+    do
+    {
+        h = get_int("hoogte: ");
+    }
+    while( h < 1 || h > 8);
+
+    for( int i = 0; i < h; i++)
+    {
+        for (int j = h - i; j > 0; j--)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k > i; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+
+        for (int k =0; k> i; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
