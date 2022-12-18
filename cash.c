040ea202413e1f62728c8a4efda192aa11f70153
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-18 12:01:48 +0000

    Sun, Dec 18, 2022, 12:01 PM +00:00

--- cash.c
+++ cash.c
@@ -1,5 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
+int main(void)
+{
+    float cash;
+    do
+    {
+        cash = get_float("change owed: ");
+    }
+    while(cash < 0);
+
+    int cents = round(cash * 100);
+
+    if(cents >= 25)
+}
 
 
