abd89ce139067f5aa9c14c86a54593db01777e5b
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-18 13:38:19 +0000

    Sun, Dec 18, 2022, 1:38 PM +00:00

--- calculator0.py
+++ calculator0.py
@@ -1,3 +1,11 @@
-x = int(input("x: "))
-y = int(input("y: "))
+try:
+    x = int(input("x: "))
+except:
+    print("ongeldig")
+    exit()
+try:
+    y = int(input("y: "))
+except:
+    print("ongeldig")
+    exit()
 print(f"het resultaat is {x + y}")
