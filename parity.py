225208a1c3e399ee5d16215bec88f94d218dd39b
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-18 13:59:11 +0000

    Sun, Dec 18, 2022, 1:59 PM +00:00

---
+++ parity.py
@@ -0,0 +1,11 @@
+try:
+    n = int(input("n: "))
+except:
+    print("Dit is geen geheel getal en kan dus niet even zijn")
+    exit()
+
+if n % 2 = 0:
+    print("Het getal is even")
+else:
+    print("Het getal is oneven")
+
