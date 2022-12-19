03167c971d39ee00cf350eb928d50caabaf592e5
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-19 16:17:18 +0000

    Mon, Dec 19, 2022, 4:17 PM +00:00

--- scores.py
+++ scores.py
@@ -1,5 +1,15 @@
 scores = []
 
 while True:
-    
-aantal = int(input("vul het aantal scores in dat u wilt ingeven: "))
+    aantal = int(input("vul het aantal scores in dat u wilt ingeven: "))
+    try:
+       if aantal > 0:
+            break
+        else:
+            print("vul een getal groter dan 0 in")
+    except:
+        print("Vul een getal groter dan 0 in")
+
+for i in range(aantal):
+    score = int(input("Score: "))
+    scores.append(score)
