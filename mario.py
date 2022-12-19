b9dae974b7e0f5dbc9c9c0707c6531b0aa3a9e32
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-19 15:33:41 +0000

    Mon, Dec 19, 2022, 3:33 PM +00:00

--- mario.py
+++ mario.py
@@ -1,9 +1,11 @@
-while True:
-    n = int(input("height: "))
-    if n > 0 and n <= 8:
-        break
-    else:
-        print("vul een geheel getal in tussen 1 en 8")
-
+def main():
 for i in range(n):
     print("#")
+
+def get height()
+    hile True:
+        n = int(input("height: "))
+        if n > 0 and n <= 8:
+            break
+        else:
+            print("vul een geheel getal in tussen 1 en 8")
