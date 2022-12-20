068153c3868d6fc61ecb971c789b18f4927e57fc
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-20 19:37:02 +0000

    Tue, Dec 20, 2022, 7:37 PM +00:00

--- marioez.py
+++ marioez.py
@@ -1,3 +1,4 @@
+def main():
 
 
 
@@ -5,16 +6,15 @@
 
 
 
+def userinput():
+    while true
+        try:
+            hoogte = int(input("Geef de hoogte in: "))
+            if hoogte < 9 and hoogte > 0:
+                break
+            else:
+                print("Geef een getal in tussen 0 en 8")
+        except:
+            print("Dit is geen getal")
 
-
-try:
-    while(true):
-        hoogte = int(input("Geef de hoogte in: "))
-        if hoogte < 9 and hoogte > 0:
-            break
-        else:
-            print("Geef een getal in tussen 0 en 8")
-except:
-    print("Dit is geen getal")
-
-print(f"{hoogte}")
+main()
