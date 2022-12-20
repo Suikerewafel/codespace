59edca7c9bcd1841381c019128156e44f8c25fb1
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-20 19:29:05 +0000

    Tue, Dec 20, 2022, 7:29 PM +00:00

--- marioez.py
+++ marioez.py
@@ -1,2 +1,15 @@
 def main():
-    
+
+
+
+
+
+
+
+def userinput():
+    try:
+        while(true):
+            hoogte = int(input("Geef de hoogte in: "))
+            if hoogte > 8 or hoogte 
+    except:
+        exit()
