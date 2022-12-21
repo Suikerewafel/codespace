5249ded6ae39ddede271f17b13b9a5b81fd5e741
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-21 16:05:15 +0000

    Wed, Dec 21, 2022, 4:05 PM +00:00

--- math_1.py
+++ math_1.py
@@ -1,4 +1,6 @@
 card_number = (input("Geef uw kaartnummer in: ")) # cijfer moet 16 nummers zijn
 every_2 = card_number[0::2]
-print(sum(every_2))
+list_every_2 = [every_2]
+print(sum(list_every_2))
+
 
