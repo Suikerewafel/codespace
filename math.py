e70d56a0573ae9f8dcd20d67cdfc8931646192b1
Suikerewafel <119307270+Suikerewafel@users.noreply.github.com>
2022-12-21 15:42:01 +0000

    Wed, Dec 21, 2022, 3:42 PM +00:00

--- math.py
+++ math.py
@@ -1,4 +1,4 @@
 import math
 card_number = int(input("Geef uw kaartnummer in: ")) # cijfer moet 16 nummers zijn
 last number = card_number % 10
-sum_2 = sum()
+sum_2 = sum(card_number[0, 2, 4, 6, 8, 10, 12, 14])
